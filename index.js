c0863a571014611f4c1c68a0d31e239023b720cd
Erik Gillespie <erik.gillespie@gmail.com>
2019-12-11 19:35:44 -0500

    Add and configure helmet, fixes #7

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const helmet = require('helmet')
+
 const app = express()
+app.use(helmet())
+
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))

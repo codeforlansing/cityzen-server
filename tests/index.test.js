ff22d4f39a62dbb07c2d3767539bdba4daaa3d67
Robin Miller <grenewodemiller@gmail.com>
2019-12-18 13:02:46 -0500

    Fix include path so that tests run succesfully

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 
 const request = require('supertest')
-const app = require('./index')
+const app = require('../index')
 
 describe('Test the root path', () => {
   test('It should respond to the GET method', () => {

7899536f3e5c7a7603d85af5dc4528e4dc34860a
Robin Miller <grenewodemiller@gmail.com>
2019-12-18 21:20:58 -0500

    Adds tests for the basic routes for #9

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,29 @@
 const supertest = require('supertest')
 const app = require('../index')
 
-describe('Test the root path', () => {
-  test('It should respond to the GET method', () => {
-    return supertest(app).get('/').expect(200, 'Hello World!')
+describe('Test that the basic routes return dummy data', () => {
+  test.skip('GET /tasks/', async () => {
+    const { body } = await supertest(app)
+      .get('/tasks/')
+      .expect('Content-Type', '/json/')
+      .expect(200)
+
+    expect(body).toEqual({
+      taskId: 'taskid',
+      name: 'Task Name',
+      description: 'Task description',
+      status: 'todo'
+    })
   })
+
+  test.skip('POST /tasks/volunteer', () => {
+    return supertest(app)
+      .post('/tasks/volunteer')
+      .send({
+        email: 'example@example.com',
+        taskIds: ['uuid']
+      })
+      .expect(200)
+  })
+  test.todo('POST /user/report')
 })

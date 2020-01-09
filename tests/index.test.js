350a170f41cbec0e45e0b50b86b6271841fc99cb
grenewode <dev@grenewode.io>
2020-01-08 20:11:55 -0500

    Adds the first simple routes & test
    This implements the GET /tasks/ route and the POST /tasks/volunteer/ routes, and adds tests.
    
    However, these currently just return dummy data. The next steps will be to
    1. Allow the application to return more meaningful data in response to these queries
    2. Allow the tests to verify that the application state has been mutated
    as expected when the routes are called

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,28 +2,28 @@ const supertest = require('supertest')
 const app = require('../index')
 
 describe('Test that the basic routes return dummy data', () => {
-  test.skip('GET /tasks/', async () => {
+  test('GET /tasks/', async () => {
     const { body } = await supertest(app)
       .get('/tasks/')
-      .expect('Content-Type', '/json/')
+      .expect('Content-Type', 'application/json; charset=utf-8')
       .expect(200)
 
-    expect(body).toEqual({
+    expect(body).toEqual([{
       taskId: 'taskid',
       name: 'Task Name',
       description: 'Task description',
       status: 'todo'
-    })
+    }])
   })
 
-  test.skip('POST /tasks/volunteer', () => {
+  test('POST /tasks/volunteer', () => {
     return supertest(app)
       .post('/tasks/volunteer')
       .send({
         email: 'example@example.com',
         taskIds: ['uuid']
       })
-      .expect(200)
+      .expect(201)
   })
 
   test.todo('POST /user/report')

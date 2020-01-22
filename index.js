b604b593f1b18ee1c89e9bb3a0e8632d566a9ef4
Medardo Antonio Rodriguez <med@merchise.org>
2020-01-22 14:19:25 -0500

    Define a constant with the port the application will run on

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require('./src/app')
 
-// This actually starts the server listening at port on our local ip address.
+// Set the port our application will run on.  Currently we are hard coding
+// this, but eventually we will want to be loading information like this from
+// a settings file for an environment variable.
+const port = 3000
+
+// This actually starts the server listening at port on our local IP address.
 app({
   // pass the application configuration here
-}).listen(3000, () => console.log(`Example app listening on port ${3000}!`))
+}).listen(port, () => console.log(`Example app listening on port ${port}!`))
